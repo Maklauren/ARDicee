b962e5d9c6be39e0698e7cb2247afbe71b2b5b73
Pavel Chernook <Maklauren97@gmail.com>
2022-08-11 17:00:54 +0300

    Import 3D model for AR

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,7 +10,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -22,11 +22,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        if let diceNote = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNote.position = SCNVector3(0, 0, -0.1)
+            
+            sceneView.scene.rootNode.addChildNode(diceNote)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,7 +36,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
